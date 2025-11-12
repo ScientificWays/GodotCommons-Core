80a756c7083b87be411e29d3020383a5b5253bc1
zana4ka <albert.bagautdinov03@mail.ru>
2025-11-12 14:22:05 +0300

    Inlined TransitionLevel2D.LoadWithTransition()

--- Scripts/Levels/ChangeLevel.gd
+++ Scripts/Levels/ChangeLevel.gd
@@ -35,4 +35,8 @@ func change_level_immediately() -> void:
 		is_transitioning = false
 		WorldGlobals.change_level_transition_finished.emit(self)
 	
-	TransitionLevel2D.LoadWithTransition(level_scene_path)
+	WorldGlobals.pending_scene_path = level_scene_path
+	
+	var _game_mode := WorldGlobals._game_state._game_mode
+	assert(not _game_mode.TransitionScenePath.is_empty())
+	WorldGlobals.load_scene_by_path(_game_mode.TransitionScenePath)
