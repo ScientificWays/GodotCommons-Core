11476c3106bd739d8b426721d64de9cfac4bcaa8
zana4ka <albert.bagautdinov03@mail.ru>
2025-11-26 19:30:01 +0300

    feat: homing rework to torque, damage area in projectile collision

--- Scripts/Projectiles/Projectile2D_Collision.gd
+++ Scripts/Projectiles/Projectile2D_Collision.gd
@@ -6,6 +6,7 @@ const receive_explosions_delay_meta: StringName = &"receive_explosions_delay"
 
 @export_category("Owner")
 @export var _owner: Projectile2D
+@export var owner_damage_area: Area2D
 
 func _ready() -> void:
 	
@@ -19,9 +20,21 @@ func _ready() -> void:
 	
 	var monitor_hits_start_delay := get_meta(monitor_hits_start_delay_meta, 0.0) as float
 	if monitor_hits_start_delay > 0.0:
-		GameGlobals.delayed_collision_activate(_owner, _on_target_entered, monitor_hits_start_delay, self)
+		
+		if owner_damage_area:
+			owner_damage_area.monitoring = false
+			GameGlobals.spawn_one_shot_timer_for(self, func():
+				if is_instance_valid(owner_damage_area):
+					owner_damage_area.monitoring = true
+			, monitor_hits_start_delay)
+		
+		GameGlobals.delayed_collision_activate(_owner, _on_projectile_body_hit, monitor_hits_start_delay, self)
 	else:
-		_owner.body_entered.connect(_on_target_entered)
+		_owner.body_entered.connect(_on_projectile_body_hit)
+		
+		if owner_damage_area:
+			owner_damage_area.area_entered.connect(_on_damage_area_target_entered)
+			owner_damage_area.body_entered.connect(_on_damage_area_target_entered)
 	
 	var receive_explosions_delay := get_meta(receive_explosions_delay_meta, 0.0) as float
 	if receive_explosions_delay > 0.0 and _owner.collision_layer & GameGlobals_Class.collision_layer_explosion_receiver:
@@ -31,27 +44,32 @@ func _ready() -> void:
 	if _owner.data.should_damage_on_hit or _owner.data.should_remove_on_hit:
 		assert(_owner.contact_monitor and _owner.max_contacts_reported > 0)
 
-func _on_target_entered(in_target: Node):
+func _on_projectile_body_hit(in_target: Node) -> void:
 	
 	var hit_speed_squared := _owner.linear_velocity.length_squared() + _owner.angular_velocity * _owner.angular_velocity * 150.0
 	if _owner.data.hit_speed_threshold > 0.0 and (hit_speed_squared < _owner.data.hit_speed_threshold * _owner.data.hit_speed_threshold):
-		return false
+		return
 	
 	var hit_speed := sqrt(hit_speed_squared)
 	if _owner.data.hit_sound_event:
 		AudioGlobals.try_play_sound_varied_at_global_position(_owner.data.sound_bank_label, _owner.data.hit_sound_event, _owner.global_position, _owner.data.get_hit_sound_pitch_mul(hit_speed), _owner.data.get_hit_sound_volume_db(hit_speed))
 	
 	if _owner.data.should_damage_on_hit:
-		
-		var target_receiver := DamageReceiver.try_get_from(in_target)
-		if target_receiver:
-			
-			var instigator_receiver := DamageReceiver.try_get_from(_owner._instigator)
-			if target_receiver != instigator_receiver:
-				
-				var target_damage := _owner.data.get_hit_damage(_owner._level)
-				target_receiver.try_receive_damage(self, _owner._instigator, target_damage, DamageReceiver.DamageType_RangedHit, false)
+		apply_impact_damage_to(in_target)
 	
 	if _owner.data.should_remove_on_hit:
 		_owner.handle_remove_from_scene(Projectile2D.RemoveReason.Hit)
-	return true
+
+func _on_damage_area_target_entered(in_target: Node) -> void:
+	apply_impact_damage_to(in_target)
+
+func apply_impact_damage_to(in_target: Node) -> void:
+	
+	var target_receiver := DamageReceiver.try_get_from(in_target)
+	if target_receiver:
+		
+		var instigator_receiver := DamageReceiver.try_get_from(_owner._instigator)
+		if target_receiver != instigator_receiver:
+			
+			var target_damage := _owner.data.get_hit_damage(_owner._level)
+			target_receiver.try_receive_damage(self, _owner._instigator, target_damage, DamageReceiver.DamageType_RangedHit, false)
--- Scripts/Projectiles/Projectile2D_Homing.gd
+++ Scripts/Projectiles/Projectile2D_Homing.gd
@@ -1,22 +1,25 @@
-extends Marker2D
+extends Area2D
 class_name Projectile2D_Homing
 
 @export_category("Owner")
 @export var owner_projectile: Projectile2D
 @export var ignore_owner_mass: bool = true
 
-@export_category("Force")
-@export var homing_force_mul_base: float = 1.0
-@export var homing_force_mul_per_level_gain: float = 0.1
+@export_category("Targeting")
+@export var ignore_instigator: bool = true
 
-const homing_force_magnitude_base: float = 500.0
+@export_category("Torque")
+@export var homing_torque_mul_base: float = 1.0
+@export var homing_torque_mul_per_level_gain: float = 0.1
 
-func get_homing_force_mul() -> float:
-	return homing_force_mul_base + homing_force_mul_per_level_gain * owner_projectile._level
+const homing_torque_magnitude_base: float = 50.0
 
-func calc_force_magnitude() -> float:
+func get_homing_torque_mul() -> float:
+	return homing_torque_mul_base + homing_torque_mul_per_level_gain * owner_projectile._level
+
+func calc_torque_magnitude() -> float:
 	
-	var out_magnitude := homing_force_magnitude_base * get_homing_force_mul()
+	var out_magnitude := homing_torque_magnitude_base * get_homing_torque_mul()
 	if ignore_owner_mass:
 		out_magnitude *= owner_projectile.mass
 	return out_magnitude
@@ -27,21 +30,27 @@ func _ready() -> void:
 	
 	assert(owner_projectile)
 	
-	owner_projectile.body_entered.connect(_on_owner_target_entered)
-	owner_projectile.body_exited.connect(_on_owner_target_exited)
+	area_entered.connect(_on_homing_target_entered)
+	body_entered.connect(_on_homing_target_entered)
+	area_exited.connect(_on_homing_target_exited)
+	body_exited.connect(_on_homing_target_exited)
 
 func _physics_process(in_delta: float) -> void:
 	
 	if not is_instance_valid(current_homing_target):
 		return
 	
-	var homing_direction := owner_projectile.global_position.direction_to(current_homing_target.global_position)
-	var homing_force := homing_direction * calc_force_magnitude()
-	owner_projectile.apply_force(homing_force, global_position - owner_projectile.global_position)
+	var homing_angle := owner_projectile.global_position.angle_to_point(current_homing_target.global_position)
+	var projectile_angle := owner_projectile.global_rotation
+	owner_projectile.apply_torque(angle_difference(projectile_angle, homing_angle) * calc_torque_magnitude())
 
-func _on_owner_target_entered(in_target: Node2D) -> void:
+func _on_homing_target_entered(in_target: Node2D) -> void:
+	
+	if ignore_instigator and in_target == owner_projectile._instigator:
+		return
+	
 	current_homing_target = in_target
 
-func _on_owner_target_exited(in_target: Node2D) -> void:
+func _on_homing_target_exited(in_target: Node2D) -> void:
 	if in_target == current_homing_target:
 		current_homing_target = null
